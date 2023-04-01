934bb2fcf83da6b65c85e4846305b42cb5f056d4
Juliennu <wsb.1099@gmail.com>
2023-04-01 23:05:16 +0900

    add swiftUI project

---
+++ SwiftUI100knocks/SwiftUI100knocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI100knocksApp.swift
+//  SwiftUI100knocks
+//
+//  Created by Juri Ohto on 2023/04/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI100knocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
