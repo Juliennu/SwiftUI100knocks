27e800bb2f91349903a449acea9bf90683404660
Juri Ohto <76898162+Juliennu@users.noreply.github.com>
2023-04-02 19:57:16 +0900

    Lesson9: Buttonが押されたら文字を変える (#12)
    * Lesson9: Buttonが押されたら文字を変える
    
    * リファクタリング
    
    * 変数から関数に変更

---
+++ SwiftUI100knocks/Lessons/Lesson9View.swift
@@ -0,0 +1,45 @@
+//
+//  Lesson9View.swift
+//  SwiftUI100knocks
+//
+//  Created by Juri Ohto on 2023/04/02.
+//
+
+// Buttonが押されたら文字を変えてください。
+
+import SwiftUI
+
+struct Lesson9View: View {
+    enum City: String {
+        case egypt = "Egypt"
+        case tokyo = "Tokyo"
+
+        func toggleCity() -> Self {
+            switch self {
+            case .egypt:
+                return .tokyo
+            case .tokyo:
+                return .egypt
+            }
+        }
+    }
+
+    @State var city: City = .egypt
+
+    var body: some View {
+        VStack(spacing: 8) {
+            Text(city.rawValue)
+            Button("Tap Me!!", action: changeText)
+        }
+    }
+
+    func changeText() {
+        city = city.toggleCity()
+    }
+}
+
+struct Lesson9View_Previews: PreviewProvider {
+    static var previews: some View {
+        Lesson9View()
+    }
+}
