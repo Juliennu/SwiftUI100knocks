53cdb861ab90ffa644a1abb4dfc2a2893cdd4a33
Juliennu <wsb.1099@gmail.com>
2023-04-21 18:11:55 +0900

    Lesson15: アラートを表示する（その2）

---
+++ SwiftUI100knocks/Lessons/Lesson15View.swift
@@ -0,0 +1,30 @@
+//
+//  Lesson15View.swift
+//  SwiftUI100knocks
+//
+//  Created by Juri Ohto on 2023/04/21.
+//
+
+import SwiftUI
+
+struct Lesson15View: View {
+    @State private var isShowingAlert = false
+
+    var body: some View {
+        Button("Tap Me!") {
+            self.isShowingAlert.toggle()
+        }
+        .alert("Title", isPresented: $isShowingAlert) {
+            Button("キャンセル", role: .cancel) {}
+            Button("削除", role: .destructive) {}
+        } message: {
+            Text("Message")
+        }
+    }
+}
+
+struct Lesson15View_Previews: PreviewProvider {
+    static var previews: some View {
+        Lesson15View()
+    }
+}
