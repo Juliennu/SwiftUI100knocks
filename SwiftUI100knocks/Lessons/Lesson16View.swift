fb793fec21fe7e0c01d8f1fcd7c2d190eea9e873
Juri Ohto <76898162+Juliennu@users.noreply.github.com>
2023-04-28 15:44:44 +0900

    Lesson16: アラートを出し分ける（その1） (#20)

---
+++ SwiftUI100knocks/Lessons/Lesson16View.swift
@@ -0,0 +1,40 @@
+//
+//  Lesson16View.swift
+//  SwiftUI100knocks
+//
+//  Created by Juri Ohto on 2023/04/28.
+//
+
+import SwiftUI
+
+struct Lesson16View: View {
+    @State private var isShowingAlert1 = false
+    @State private var isShowingAlert2 = false
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            Button("Button1") {
+                isShowingAlert1.toggle()
+            }
+            .alert("Alert1", isPresented: $isShowingAlert1) {
+                Button("Close") {}
+            } message: {
+                Text("Message")
+            }
+            Button("Button2") {
+                isShowingAlert2.toggle()
+            }
+            .alert("Alert2", isPresented: $isShowingAlert2) {
+                Button("Close") {}
+            } message: {
+                Text("Message")
+            }
+        }
+    }
+}
+
+struct Lesson16View_Previews: PreviewProvider {
+    static var previews: some View {
+        Lesson16View()
+    }
+}
