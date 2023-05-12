4b107f95d2134f66eae5a868c76d875f6c511053
Juliennu <wsb.1099@gmail.com>
2023-05-12 17:22:34 +0900

    Lesson18: Button内の画像やテキストの色を変えない

---
+++ SwiftUI100knocks/Lessons/Lesson18View.swift
@@ -0,0 +1,38 @@
+//
+//  Lesson18View.swift
+//  SwiftUI100knocks
+//
+//  Created by Juri Ohto on 2023/05/12.
+//
+
+import SwiftUI
+
+struct Lesson18View: View {
+    @State private var title: String = "Pyramid"
+
+    var body: some View {
+        VStack(spacing: 24) {
+            Text(title)
+            Button(action: {
+                title = "Forever"
+            }) {
+                HStack(spacing: 16) {
+                    Image("pyramid")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 44, height: 44)
+                    Text("Tap Me!!")
+                }
+            }
+            .accentColor(.black)
+            .padding()
+            .border(.gray, width: 2)
+        }
+    }
+}
+
+struct Lesson18View_Previews: PreviewProvider {
+    static var previews: some View {
+        Lesson18View()
+    }
+}
